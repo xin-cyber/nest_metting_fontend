27afecde8c59a1ac805641d048588b7a9c58534a
jx <jinxincandor@163.com>
2023-08-13 19:00:23 +0800

    Feat：初始化

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,35 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter, Link, Outlet } from 'react-router-dom';
+import { Register } from './page/register/Register';
+import { Login } from './page/login/Login';
+import { UpdatePassword } from './page/update_password/UpdatePassword';
+import { ErrorPage } from './page/error/ErrorPage';
+
+const routes = [
+  {
+    path: "/",
+    element: <div>index</div>,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "login",
+    element: <Login />,
+  },
+  {
+    path: "register",
+    element: <Register />,
+  },
+  {
+    path: "update_password",
+    element: <UpdatePassword />,
+  }
+];
+const router = createBrowserRouter(routes);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<RouterProvider router={router}/>);
+
